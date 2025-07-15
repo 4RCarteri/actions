40711a5b03ff0cfffcce90708ee35cb627ca1c41
Ricardo Uehlein <ricardo.uehlein@wexinc.com>
2025-07-15 15:35:09 -0300

    Add EdgeDataConversion stored procedure for WEX import process

--- Databases/ODS/Stored Views/CompanyPrimaryAddress.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR ALTER VIEW [dbo].[CompanyPrimaryAddress]
- 
-AS
-SELECT DISTINCT        c.CompanyID, c.CompanyName, a.StreetAddress, a.City, a.State, a.PostalCode, a.Country
-FROM            dbo.Company AS c INNER JOIN
-                         dbo.CompanyAddress AS ca ON ca.CompanyID = c.CompanyID AND ca.IsPrimary = 1 INNER JOIN
-                         dbo.Address AS a ON a.AddressID = ca.AddressID
-
