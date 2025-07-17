4e35b8f17e41c680be7d25a7f7d4ed6c61d1519c
4RCarteri <133902159+4RCarteri@users.noreply.github.com>
2025-07-17 15:54:34 -0300

    Update Databases/hotels/Stored_Procedures/spGetSingleGoldInvoiceForSalesForce.sql
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Databases/hotels/Stored_Procedures/spGetSingleGoldInvoiceForSalesForce.sql
+++ Databases/hotels/Stored_Procedures/spGetSingleGoldInvoiceForSalesForce.sql
@@ -35,27 +35,27 @@ Select	dc.SalesforceID as Dealer
 		,payit.InvoiceTotal as PayableAmount
 		,payit.InvoiceTax as PayableTax
 
-From	ResellerInvoice reci
-		Inner Join ResellerInvoice payi on payi.PurchaseOrderID = reci.PurchaseOrderID
-		Inner Join ResellerInvoiceTotals recit on recit.ResellerInvoiceID = reci.ResellerInvoiceID
-		Inner Join ResellerInvoiceTotals payit on payit.ResellerInvoiceID = payi.ResellerInvoiceID
-		Left Join TireCount recTire on recTire.ResellerInvoiceID = reci.ResellerInvoiceID
-		Left Join TireCount payTire on payTire.ResellerInvoiceID = payi.ResellerInvoiceID
-		Inner Join Dealer d on d.DealerID = reci.DealerID
-		Inner Join Company dc on dc.CompanyID = d.CompanyID
-		Inner Join Member m on m.MemberID = reci.MemberID
-		Inner Join Company mc on mc.CompanyID = m.CompanyID
-		Inner JOIN PurchaseOrder po on po.PurchaseOrderID = reci.PurchaseOrderID
-		Left Join ResellerVendorInvoice vi on vi.PurchaseOrderNumber = reci.InvoiceNumber and vi.ResellerVendorInvoiceStatusID = 2
-		Inner Join PurchaseOrderCreditVendor pocv on pocv.PurchaseOrderID = reci.PurchaseOrderID
-		Inner Join PurchaseOrderPrimaryVendor popv on popv.PurchaseOrderID = payi.PurchaseOrderID
-		Inner Join Vendor v on v.VendorID = popv.PrimaryVendorID
-		Inner Join Company vc on vc.CompanyID = v.CompanyID
-		Inner Join CompanyAddress mca on mca.CompanyID = m.CompanyID
-		Inner Join [Address] ma on ma.AddressID = mca.AddressID
-		Inner Join RegionState rs on rs.StateCode = ma.[State]
-		Inner Join Region r on r.RegionID = rs.RegionID
-		Inner Join [User] u on u.UserID = r.TireSpecialistUserID
+FROM ResellerInvoice AS reci
+INNER JOIN ResellerInvoice AS payiON reci.PurchaseOrderID = payi.PurchaseOrderID
+INNER JOIN ResellerInvoiceTotals AS recitON reci.ResellerInvoiceID = recit.ResellerInvoiceID
+INNER JOIN ResellerInvoiceTotals AS payitON payi.ResellerInvoiceID = payit.ResellerInvoiceID
+LEFT JOIN TireCount AS recTireON reci.ResellerInvoiceID = recTire.ResellerInvoiceID
+LEFT JOIN TireCount AS payTireON payi.ResellerInvoiceID = payTire.ResellerInvoiceID
+INNER JOIN Dealer AS dON reci.DealerID = d.DealerID
+INNER JOIN Company AS dcON d.CompanyID = dc.CompanyID
+INNER JOIN Member AS mON reci.MemberID = m.MemberID
+INNER JOIN Company AS mcON m.CompanyID = mc.CompanyID
+INNER JOIN PurchaseOrder AS poON reci.PurchaseOrderID = po.PurchaseOrderID
+LEFT JOIN ResellerVendorInvoice AS viON reci.InvoiceNumber = vi.PurchaseOrderNumber AND vi.ResellerVendorInvoiceStatusID = 2
+INNER JOIN PurchaseOrderCreditVendor AS pocvON reci.PurchaseOrderID = pocv.PurchaseOrderID
+INNER JOIN PurchaseOrderPrimaryVendor AS popvON payi.PurchaseOrderID = popv.PurchaseOrderID
+INNER JOIN Vendor AS vON popv.PrimaryVendorID = v.VendorID
+INNER JOIN Company AS vcON v.CompanyID = vc.CompanyID
+INNER JOIN CompanyAddress AS mcaON m.CompanyID = mca.CompanyID
+INNER JOIN [Address] AS maON mca.AddressID = ma.AddressID
+INNER JOIN RegionState AS rsON ma.[State] = rs.StateCode
+INNER JOIN Region AS rON rs.RegionID = r.RegionID
+INNER JOIN [User] AS uON r.TireSpecialistUserID = u.UserID
 
 WHERE po.PurchaseOrderNumber = @PONumber
   AND reci.ResellerInvoiceTypeID = 2
