91ba1bca2430baa11a6f5c5596304d2694c91fdc
4RCarteri <133902159+4RCarteri@users.noreply.github.com>
2025-07-17 16:38:38 -0300

    Update Databases/ODS/Stored_Procedures/test.sql
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Databases/ODS/Stored_Procedures/test.sql
+++ Databases/ODS/Stored_Procedures/test.sql
@@ -1,1 +1,4 @@
-SELECT  a.name,  c.names FROM sys.procedures AS a INNER JOIN  sys.sql_modules AS b ON a.object_id = b.object_id AND a.name = 'ODSCleanup'
+SELECT
+  a.name,
+  c.names
+FROM sys.procedures AS a INNER JOIN sys.sql_modules AS b ON a.object_id = b.object_id AND a.name = 'ODSCleanup'
