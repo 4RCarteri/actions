c9022467f14b7f8366acf9bbf558a41f4bc8e6cf
Ricardo Uehlein <ricardo.uehlein@wexinc.com>
2025-07-17 16:49:21 -0300

    Update SQL queries in test.sql for improved selection and clarity

--- Databases/ODS/Stored_Procedures/test.sql
+++ Databases/ODS/Stored_Procedures/test.sql
@@ -1,2 +1,1 @@
-SELECT a.name
-FROM sys.procedures AS a INNER JOIN sys.sql_modules AS b ON a.object_id = b.object_id AND a.name = 'ODSCleanup'
+SELECT  a.name,  c.names FROM sys.procedures AS a INNER JOIN  sys.sql_modules AS b ON a.object_id = b.object_id AND a.name = 'ODSCleanup'
