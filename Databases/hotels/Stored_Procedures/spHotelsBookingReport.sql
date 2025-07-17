2631b678161de6e7e90202de0657b2995baec39e
Ricardo Uehlein <ricardo.uehlein@wexinc.com>
2025-07-17 14:50:29 -0300

    Fix casing in SQL procedure output for consistency and ensure 'level' parameter is set in SQLFluff workflow

--- Databases/hotels/Stored_Procedures/spHotelsBookingReport.sql
+++ Databases/hotels/Stored_Procedures/spHotelsBookingReport.sql
@@ -4,11 +4,11 @@ CREATE PROCEDURE [dbo].[spHotelsBookingReport]
 
 AS
 Select
-		format(o.DateCreated, 'MM/dd/yyyy') AS 'Hotel Booking Date', -- src.Payment.Order.DateCreated.ToString(dateFormat)
-		o.AccountNumber AS 'Account Number', -- src.Payment.Order.AccountNumber
-		pmt.TransactionType AS 'Transaction Type', -- src.TransactionType
-		format(o.StartDate, 'MM/dd/yyyy') AS 'Hotel Check In Date', -- src.Payment.Order.StartDate.ToString(dateFormat)
-		format(o.EndDate, 'MM/dd/yyyy') aS 'Check Out Date', -- src.Payment.Order.EndDate.ToString(dateFormat)
+		format(o.DateCreated, 'MM/dd/yyyy') as 'Hotel Booking Date', -- src.Payment.Order.DateCreated.ToString(dateFormat)
+		o.AccountNumber as 'Account Number', -- src.Payment.Order.AccountNumber
+		pmt.TransactionType as 'Transaction Type', -- src.TransactionType
+		format(o.StartDate, 'MM/dd/yyyy') as 'Hotel Check In Date', -- src.Payment.Order.StartDate.ToString(dateFormat)
+		format(o.EndDate, 'MM/dd/yyyy') as 'Check Out Date', -- src.Payment.Order.EndDate.ToString(dateFormat)
 		ppty.[Name] as 'Hotel Name', -- src.Payment.Order.Property.Name
 		ppty.Street1 + ' ' + ppty.Street2 + ' ' + ppty.Street3  as 'Hotel Address', -- src.Payment.Order.Property.Street1 + " " + src.Payment.Order.Property.Street2 + " " + src.Payment.Order.Property.Street3
 		ppty.City as 'Hotel City', -- src.Payment.Order.Property.City
