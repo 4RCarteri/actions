d0bf154f14aa9acf00658d0de06b1f55c483863a
Ricardo Uehlein <ricardo.uehlein@wexinc.com>
2025-07-17 16:13:20 -0300

    Remove spGetSingleGoldInvoiceForSalesForce procedure and add ODSCleanup procedure for log and address cleanup

---
+++ Databases/ODS/Stored_Procedures/ODSCleanup.sql
@@ -0,0 +1,31 @@
+CREATE OR ALTER PROCEDURE [dbo].[ODSCleanup]
+
+AS
+BEGIN
+			-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Clear out our error logs
+	DELETE FROM ApplicationError
+	WHERE ErrorDate < DATEADD(MONTH, -1, GETDATE())
+
+	--clear oulder log entries
+	DELETE 
+	FROM ApplicationLog
+	WHERE LogDate > DATEADD(DAY, -90, GETDATE())
+
+	-- Delete any addresses that aren't connected anymore
+	DELETE FROM Address
+	WHERE AddressID IN((
+	SELECT a.AddressID
+	FROM Address a
+		LEFT OUTER JOIN CompanyAddress ca
+			ON ca.AddressID = a.AddressID
+		LEFT OUTER JOIN ContactAddress cca
+			ON cca.AddressID = a.AddressID
+			      LEFT OUTER JOIN PersonAddress AS pa
+        ON a.AddressID = pa.AddressID
+	WHERE ca.CompanyAddressID IS NULL AND cca.ContactAddressID IS NULL))
+
+END
