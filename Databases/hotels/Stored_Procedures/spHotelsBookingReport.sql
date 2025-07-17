e3e7696f4942126fdf6d4539c5beed90b8d240cf
Ricardo Uehlein <ricardo.uehlein@wexinc.com>
2025-07-17 14:40:13 -0300

    Update SQLFluff version and refine SQL procedure formatting for consistency

--- Databases/hotels/Stored_Procedures/spHotelsBookingReport.sql
+++ Databases/hotels/Stored_Procedures/spHotelsBookingReport.sql
@@ -7,7 +7,7 @@ Select
 		format(o.DateCreated, 'MM/dd/yyyy') AS 'Hotel Booking Date', -- src.Payment.Order.DateCreated.ToString(dateFormat)
 		o.AccountNumber AS 'Account Number', -- src.Payment.Order.AccountNumber
 		pmt.TransactionType AS 'Transaction Type', -- src.TransactionType
-		format(o.StartDate, 'MM/dd/yyyy') As 'Hotel Check In Date', -- src.Payment.Order.StartDate.ToString(dateFormat)
+		format(o.StartDate, 'MM/dd/yyyy') AS 'Hotel Check In Date', -- src.Payment.Order.StartDate.ToString(dateFormat)
 		format(o.EndDate, 'MM/dd/yyyy') as 'Check Out Date', -- src.Payment.Order.EndDate.ToString(dateFormat)
 		ppty.[Name] as 'Hotel Name', -- src.Payment.Order.Property.Name
 		ppty.Street1 + ' ' + ppty.Street2 + ' ' + ppty.Street3  as 'Hotel Address', -- src.Payment.Order.Property.Street1 + " " + src.Payment.Order.Property.Street2 + " " + src.Payment.Order.Property.Street3
