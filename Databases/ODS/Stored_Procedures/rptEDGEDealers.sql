b66128230716b7ba86d1e510f3f204b476ee94eb
4RCarteri <133902159+4RCarteri@users.noreply.github.com>
2025-07-18 10:15:08 -0300

    Update Databases/ODS/Stored_Procedures/rptEDGEDealers.sql
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Databases/ODS/Stored_Procedures/rptEDGEDealers.sql
+++ Databases/ODS/Stored_Procedures/rptEDGEDealers.sql
@@ -5,35 +5,60 @@ BEGIN
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
 
-    SELECT d.DealerID AS TruckersB2BDealerID
-			,dc.CompanyName AS DealerName
-			,a.StreetAddress
-			,a.City
-			,a.State
-			,a.PostalCode
-			,dn.DealerNetworkName AS TruckersB2BNetworkIdentifier
-			,(SELECT TOP 1 Identifier FROM DealerIdentifier di WHERE di.DealerID = d.DealerID AND di.DealerIdentifierTypeID = 1) AS FTSLocationID
-			,(SELECT TOP 1 Identifier FROM DealerIdentifier di WHERE di.DealerID = d.DealerID AND di.DealerIdentifierTypeID = 2) AS FTSChainID
-			,(SELECT TOP 1 Identifier FROM DealerIdentifier di WHERE di.DealerID = d.DealerID AND di.DealerIdentifierTypeID = 7) AS FTSAffiliate
-			,(SELECT TOP 1 Identifier FROM DealerIdentifier di WHERE di.DealerID = d.DealerID AND di.DealerIdentifierTypeID = 10) AS FleetOneMerchantCode
-			,(SELECT TOP 1 Identifier FROM DealerIdentifier di WHERE di.DealerID = d.DealerID AND di.DealerIdentifierTypeID = 5) AS FleetOneDealerKey
-			,(SELECT TOP 1 Identifier FROM DealerIdentifier di WHERE di.DealerID = d.DealerID AND di.DealerIdentifierTypeID = 6) AS EFSDealerKey
-			,(SELECT TOP 1 Identifier FROM DealerIdentifier di WHERE di.DealerID = d.DealerID AND di.DealerIdentifierTypeID = 4) AS ComdataDealerKey
-			,(SELECT TOP 1 Identifier FROM DealerIdentifier di WHERE di.DealerID = d.DealerID AND di.DealerIdentifierTypeID = 3) AS TChekDealerKey
-	FROM FuelTransaction ft
-		INNER JOIN Dealer d
-			ON d.DealerID = ft.DealerID
-		INNER JOIN Company dc
-			ON dc.CompanyID = d.CompanyID
-		INNER JOIN CompanyAddress da
-			ON da.CompanyID = dc.CompanyID
-			AND da.IsPrimary = 1
-		INNER JOIN Address a
-			ON a.AddressID = da.AddressID
-		LEFT OUTER JOIN DealerNetworkAffiliation dna
-			ON dna.DealerID = d.DealerID
-		LEFT OUTER JOIN DealerNetwork dn
-			ON dn.DealerNetworkID = dna.DealerNetworkID
-	WHERE ft.ProgramID = 5
-	GROUP BY d.DealerID, dc.CompanyName, a.StreetAddress, a.City, a.State, a.PostalCode, dn.DealerNetworkName
+    SELECT
+        d.dealerid                                        AS truckersb2bdealerid,
+        dc.companyname                                    AS dealername,
+        a.streetaddress,
+        a.city,
+        a.state,
+        a.postalcode,
+        dn.dealernetworkname                              AS truckersb2bnetworkidentifier,
+        (
+            SELECT TOP 1 identifier FROM dealeridentifier AS di
+            WHERE di.dealerid = d.dealerid AND di.dealeridentifiertypeid = 1
+        )                                                 AS ftslocationid,
+        (
+            SELECT TOP 1 identifier FROM dealeridentifier AS di
+            WHERE di.dealerid = d.dealerid AND di.dealeridentifiertypeid = 2
+        )                                                 AS ftschainid,
+        (
+            SELECT TOP 1 identifier FROM dealeridentifier AS di
+            WHERE di.dealerid = d.dealerid AND di.dealeridentifiertypeid = 7
+        )                                                 AS ftsaffiliate,
+        (
+            SELECT TOP 1 identifier FROM dealeridentifier AS di
+            WHERE di.dealerid = d.dealerid AND di.dealeridentifiertypeid = 10
+        )                                                 AS fleetonemerchantcode,
+        (
+            SELECT TOP 1 identifier FROM dealeridentifier AS di
+            WHERE di.dealerid = d.dealerid AND di.dealeridentifiertypeid = 5
+        )                                                 AS fleetonedealerkey,
+        (
+            SELECT TOP 1 identifier FROM dealeridentifier AS di
+            WHERE di.dealerid = d.dealerid AND di.dealeridentifiertypeid = 6
+        )                                                 AS efsdealerkey,
+        (
+            SELECT TOP 1 identifier FROM dealeridentifier AS di
+            WHERE di.dealerid = d.dealerid AND di.dealeridentifiertypeid = 4
+        )                                                 AS comdatadealerkey,
+        (
+            SELECT TOP 1 identifier FROM dealeridentifier AS di
+            WHERE di.dealerid = d.dealerid AND di.dealeridentifiertypeid = 3
+        )                                                 AS tchekdealerkey
+    FROM fueltransaction AS ft
+    INNER JOIN dealer AS d
+        ON ft.dealerid = d.dealerid
+    INNER JOIN company AS dc
+        ON d.companyid = dc.companyid
+    INNER JOIN companyaddress AS da
+        ON dc.companyid = da.companyid
+        AND da.isprimary = 1
+    INNER JOIN address AS a
+        ON da.addressid = a.addressid
+    LEFT OUTER JOIN dealernetworkaffiliation AS dna
+        ON d.dealerid = dna.dealerid
+    LEFT OUTER JOIN dealernetwork AS dn
+        ON dna.dealernetworkid = dn.dealernetworkid
+    WHERE ft.programid = 5
+    GROUP BY d.dealerid, dc.companyname, a.streetaddress, a.city, a.state, a.postalcode, dn.dealernetworkname
 END
