83f0232cf4e6a59c6e25cac02db717a4ec06b8eb
4RCarteri <133902159+4RCarteri@users.noreply.github.com>
2025-07-18 10:15:18 -0300

    Update Databases/ODS/Stored_Procedures/rptEDGEDealers.sql
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Databases/ODS/Stored_Procedures/rptEDGEDealers.sql
+++ Databases/ODS/Stored_Procedures/rptEDGEDealers.sql
@@ -1,9 +1,9 @@
 CREATE OR ALTER PROCEDURE [dbo].[rptEDGEDealers]
 AS
 BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
-	SET NOCOUNT ON;
+    -- SET NOCOUNT ON added to prevent extra result sets from
+    -- interfering with SELECT statements.
+    SET NOCOUNT ON;
 
     SELECT
         d.dealerid                                        AS truckersb2bdealerid,
